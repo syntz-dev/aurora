e76389b4d348243835c32d580dfeb91b60944300
Braux <83966759+brauxx@users.noreply.github.com>
2023-07-17 15:50:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,50 +3,58 @@ const chatMessages = document.getElementById('chat-messages');
 let lastMessage = null;
 
 inputField.addEventListener('keydown', (event) => {
-if (event.key === 'Enter') {
-const query = inputField.value;
+ if (event.key === 'Enter') {
+   const query = inputField.value;
 
-const data = {
-text : query,
-key : '931605b9-87c1-45af-b9b8-b59b8f5ad17a',
-user_id : 'website-user',
-speak : false
-};
+   const data = {
+     text: query,
+     key: '931605b9-87c1-45af-b9b8-b59b8f5ad17a',
+     user_id: 'website-user',
+     speak: false
+   };
 
-fetch('https://api.carterlabs.ai/chat', {
-method : 'POST',
-headers : {'Content-Type' : 'application/json'},
-body : JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
+   fetch('https://api.carterlabs.ai/chat', {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/json',
+     },
+     body: JSON.stringify(data),
+   })
+     .then(response => response.json())
+     .then(data => {
+       console.log('Input:', data.input);
+       console.log('Output:', data.output);
 
-// Create a new message element
-const messageElement = document.createElement('div');
-messageElement.classList.add('message');
-messageElement.textContent = data.output.text;
+       // Create a new message element
+       const messageElement = document.createElement('div');
+       messageElement.classList.add('message');
+       messageElement.textContent = data.output.text;
 
-// Append the message element to the chat messages container
-chatMessages.appendChild(messageElement);
+       // Append the message element to the chat messages container
+       chatMessages.appendChild(messageElement);
 
-// Scroll to the bottom of the chat messages container
-chatMessages.scrollTop = chatMessages.scrollHeight;
+       // Scroll to the bottom of the chat messages container
+       chatMessages.scrollTop = chatMessages.scrollHeight;
 
-// Fade in the message element
-setTimeout(() => {messageElement.style.display = 'block';},100);
+       // Fade in the message element
+       setTimeout(() => {
+         messageElement.style.display = 'block';
+       },100);
 
-// Fade out the last message
-if (lastMessage) {
-lastMessage.classList.add('fade-out');
-setTimeout(() => {lastMessage.remove();},500);
-}
+       // Fade out the last message
+       if (lastMessage) {
+         lastMessage.classList.add('fade-out');
+         setTimeout(() => {lastMessage.remove();},1000);
+       }
 
-// Set the current message as the last message
-lastMessage = messageElement;
+       // Set the current message as the last message
+       lastMessage = messageElement;
 
-// Clear the input field
-inputField.value = '';
-})
-.catch(error => {console.error('Error:', error);});
-}
+       // Clear the input field
+       inputField.value = '';
+     })
+     .catch(error => {
+       console.error('Error:', error);
+     });
+ }
 });
